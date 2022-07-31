1d147b15e71fb3d91a4ce68b4f1cdb44822b917d
Payton <pjtrosclair@ksu.edu>
2022-07-30 23:41:14 -0500

    Added the gear page

--- AspNET4.8-Website/AspNET4.8-Website/Controllers/MusicController.cs
+++ AspNET4.8-Website/AspNET4.8-Website/Controllers/MusicController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Website.Models;
 
-namespace TailWindTest.Controllers
+namespace Website.Controllers
 {
     public class MusicController : Controller
     {
@@ -13,6 +13,16 @@ namespace TailWindTest.Controllers
         public ActionResult SelectedSounds()
         {
             return View(new AlbumCollection());
+        }
+
+        public ActionResult Gear()
+        {
+            return View(new GearCollection());
+        }
+
+        public ActionResult RedirectToYoutube(string url)
+        {
+            return Redirect(url);
         }
     }
 }
